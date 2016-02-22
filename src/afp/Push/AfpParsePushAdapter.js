da74f400ec6333fba4cdf1d1becaacbd3ecd9870
Frederic BOLTZ <frederic.boltz@gmail.com>
2016-02-22 17:19:47 +0100

    Progress multi push

--- src/afp/Push/AfpParsePushAdapter.js
+++ src/afp/Push/AfpParsePushAdapter.js
@@ -68,30 +68,36 @@ AfpParsePushAdapter.prototype.getValidPushTypes = function () {
 }
 
 AfpParsePushAdapter.prototype.send = function (data, installations) {
-	let deviceMap = classifyInstallation(installations, this.validPushTypes);
-
+	let appIdentifier = data.appIdentifier;
+	let deviceMap = this.classifyInstallation(appIdentifier, installations);
+	let appIdentifierSender = this.senderMapByAppId[appIdentifier];
 	let sendPromises = [];
 
-	for (let pushType in deviceMap) {
-		let appIdentifier = pushType["appIdentifier"];
-		let appIdentifierSender = this.senderMapByAppId[appIdentifier];
+	if (!appIdentifierSender) {
+		console.log('Can not find appIdentifier sender for appId %s, %j', appIdentifier, data);
+	}
+	else {
+		for (let pushType in deviceMap) {
+			let devices = deviceMap[pushType];
 
-		if (!appIdentifierSender) {
-			console.log('Can not find appIdentifier sender for appId %s, %j', appIdentifierSender, data);
-			continue;
+			// Should be an array of AfpAPNS or AfpGCM
+			let senders = appIdentifierSender[pushType];
+
+			if (!senders) {
+				console.log('Can not find sender for push type %s, %j', pushType, data);
+				continue;
+			}
+
+			if (Array.isArray(senders)) {
+				for (var index = 0; index < senders.length; index++) {
+					let sender = senders[index];
+
+					sendPromises.push(sender.send(data, devices));
+				}
+			} else {
+				sendPromises.push(senders.send(data, devices));
+			}
 		}
-
-		// Should be AfpAPNS or AfpGCM
-		let sender = appIdentifierSender[pushType];
-
-		if (!sender) {
-			console.log('Can not find sender for push type %s, %j', pushType, data);
-			continue;
-		}
-
-		let devices = deviceMap[pushType];
-
-		sendPromises.push(sender.send(data, devices));
 	}
 
 	return Parse.Promise.when(sendPromises);
@@ -103,12 +109,17 @@ AfpParsePushAdapter.prototype.send = function (data, installations) {
  * @param {Array} validPushTypes An array of valid push types(string)
  * @returns {Object} A map whose key is device type and value is an array of device
  */
-function classifyInstallation(installations, validPushTypes) {
+AfpParsePushAdapter.prototype.classifyInstallation = function classifyInstallation(appIdentifier, installations) {
 	// Init deviceTokenMap, create a empty array for each valid pushType
 	let deviceMap = {};
 
-	for (let validPushType of validPushTypes) {
-		deviceMap[validPushType] = [];
+	// Check if configured for this appIdentifier
+	if (!this.senderMapByAppId[appIdentifier]) {
+		return deviceMap;
+	}
+
+	for (let pushType of this.validPushTypes) {
+		deviceMap[pushType] = [];
 	}
 
 	for (let installation of installations) {
@@ -117,27 +128,26 @@ function classifyInstallation(installations, validPushTypes) {
 			continue;
 		}
 
-		// Check if the sender exists
-		if (!this.senderMapByAppId[installation.appIdentifier])
-		{
-			let pushType = installation.deviceType;
+		// Check if the good appIdentifier
+		if (installation.appIdentifier != appIdentifier) {
+			continue;
+		}
 
-			if (deviceMap[pushType]) {
-				deviceMap[pushType].push({
-					deviceType: installation.deviceType,
-					deviceToken: installation.deviceToken,
-					appIdentifier: installation.appIdentifier
-				});
-			} else {
-				console.log('Unknown push type from installation %j', installation);
-			}
+		// Check if the sender exists
+		let pushType = installation.deviceType;
+
+		if (deviceMap[pushType]) {
+			deviceMap[pushType].push({
+				deviceType: installation.deviceType,
+				deviceToken: installation.deviceToken,
+				appIdentifier: installation.appIdentifier
+			});
+		} else {
+			console.log('Unknown push type from installation %j', installation);
 		}
 	}
 
 	return deviceMap;
 }
 
-if (typeof process !== 'undefined' && process.env.NODE_ENV === 'test') {
-	AfpParsePushAdapter.classifyInstallation = classifyInstallation;
-}
 module.exports = AfpParsePushAdapter;
